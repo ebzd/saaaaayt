15b1daa771701dfe5cfa757dff9d7e4b48bd34e4
ebzd <Vlad-viper@bk.ru>
2024-03-31 15:06:16 +0700

    tochno vse

--- js/media.js
+++ js/media.js
@@ -26,7 +26,7 @@ wavesurfer.init({
 
 
 //загружаем нужную песню
-wavesurfer.load('../audio/Ohio.mp3');
+wavesurfer.load('../audio/pashalko.mp3');
 
 //play and pause a player
 playPause.addEventListener("click", function (e) {
